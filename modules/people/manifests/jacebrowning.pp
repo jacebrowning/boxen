e890ac130c2775d85e8bf1bd11cd7b9e990efcf3
Jace Browning <jacebrowning@gmail.com>
2015-03-16 13:11:18 -0400

    Add links to dotfiles and sublime settings
    Fixes #1

--- modules/people/manifests/jacebrowning.pp
+++ modules/people/manifests/jacebrowning.pp
@@ -6,4 +6,22 @@ class people::jacebrowning {
   include daisy_disk
   include appcleaner
 
+  repository { "/Users/${::boxen_user}/.dotfiles":
+      source => 'jacebrowning/dotfiles',
+      provider => git
+  }
+  file { "${boxen::config::srcdir}/dotfiles":
+    ensure => link,
+    target => "/Users/${::boxen_user}/.dotfiles"
+  }
+
+  repository { "/Users/${::boxen_user}/Library/Application Support/Sublime Text 3/Packages/User":
+    source => 'jacebrowning/sublime-settings',
+    provider => git
+  }
+  file { "${boxen::config::srcdir}/sublime-settings":
+    ensure => link,
+    target => "/Users/${::boxen_user}/Library/Application Support/Sublime Text 3/Packages/User"
+  }
+
 }
