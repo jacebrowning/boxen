1fa3c298b39f3adf939ff5744323f00b6baf8c26
Jace Browning <jacebrowning@gmail.com>
2015-03-16 15:59:47 -0400

    Remove reliance on pip

--- manifests/python.pp
+++ manifests/python.pp
@@ -9,11 +9,11 @@ package { 'python3':
 }
 $python_libs = "pep8 pep8radius pep257 pylint nose pytest coverage"
 exec { "install common python2 libraries":
-  command => "pip2 install --upgrade $python_libs",
+  command => "python2 -m pip install --upgrade $python_libs",
   require => Package['python'],
 }
 exec { "install common python3 libraries":
-  command => "pip3 install --upgrade $python_libs gdm doorstop",
+  command => "python3 -m pip install --upgrade $python_libs gdm doorstop",
   require => Package['python3'],
 }
 
