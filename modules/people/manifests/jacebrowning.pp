90fdc20e60433947deb32d5e629e1a9089f9bd12
Jace Browning <jacebrowning@gmail.com>
2015-04-27 16:03:24 -0400

    Add DiffMerge and refactor install list

--- modules/people/manifests/jacebrowning.pp
+++ modules/people/manifests/jacebrowning.pp
@@ -1,24 +1,8 @@
 class people::jacebrowning {
 
+  # shell
   include zsh
   include ohmyzsh
-
-  include iterm2::dev
-
-  include github_for_mac
-  include sourcetree
-  include p4merge
-
-  include sublime_text
-  include dia
-  package { 'cronnix': provider => 'brewcask' }
-
-  include daisy_disk
-  include appcleaner
-  include spectacle
-  package { 'skype': provider => 'brewcask' }
-  package { 'macpilot': provider => 'brewcask' }
-
   repository { "/Users/${::boxen_user}/.dotfiles":
       source => 'jacebrowning/dotfiles',
       provider => git
@@ -31,6 +15,19 @@ class people::jacebrowning {
     command => "/usr/bin/make -C /Users/${::boxen_user}/.dotfiles"
   }
 
+  # terminal
+  include iterm2::dev
+
+  # version control
+  include github_for_mac
+  include sourcetree
+  include p4merge
+  package { 'diffmerge': provider => brewcask }
+
+  # development
+  include dia
+  package { 'cronnix': provider => 'brewcask' }
+  include sublime_text
   repository { "/Users/${::boxen_user}/Library/Application Support/Sublime Text 3/Packages/User":
     source => 'jacebrowning/sublime-settings',
     provider => git
@@ -39,5 +36,12 @@ class people::jacebrowning {
     ensure => link,
     target => "/Users/${::boxen_user}/Library/Application Support/Sublime Text 3/Packages/User"
   }
+
+  # applications
+  include daisy_disk
+  include appcleaner
+  include spectacle
+  package { 'skype': provider => 'brewcask' }
+  package { 'macpilot': provider => 'brewcask' }
 
 }
