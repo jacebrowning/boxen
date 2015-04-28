07f0f21b008f9c7676fc1af399ec1d312ad67a0c
Jace Browning <jacebrowning@gmail.com>
2015-04-28 15:13:34 -0400

    Add Ember CLI

--- manifests/site.pp
+++ manifests/site.pp
@@ -79,6 +79,7 @@ node default {
   nodejs::version { 'v0.12': }
   class { 'nodejs::global': version => 'v0.12' }
   nodejs::module { 'bower': node_version => 'v0.12' }
+  nodejs::module { 'ember-cli': node_version => 'v0.12' }
   include $ios
 
   # web browsers
