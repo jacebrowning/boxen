1676525f63b91069d39cfba70795adf0817a7a1c
Jace Browning <jacebrowning@gmail.com>
2015-09-02 15:47:26 -0400

    Remove pip installers that won't work

--- manifests/python.pp
+++ manifests/python.pp
@@ -9,15 +9,6 @@ class { 'python::global':
   version => "$python3"
 }
 
-# Install common tools
-$python_libs = "pip virtualenv"
-exec { "install common python2 libraries":
-  command => "pip2 install --upgrade $python_libs ansible",
-}
-exec { "install common python3 libraries":
-  command => "pip3 install --upgrade $python_libs doorstop gdm mine",
-}
-
 # Install documentation tools
 package { 'pandoc':
   ensure => installed,
