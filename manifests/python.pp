c1e2150984772c55287bd392c6973acac17f5a10
Jace Browning <jacebrowning@gmail.com>
2015-09-02 00:45:43 -0400

    Switch to pyenv
    Fixes #2

--- manifests/python.pp
+++ manifests/python.pp
@@ -1,23 +1,24 @@
-# python versions
-package { 'python':
-  ensure => installed,
-  provider => homebrew,
-}
-package { 'python3':
-  ensure => installed,
-  provider => homebrew,
-}
-$python_libs = "pip virtualenv pep8 pep8radius pep257 pylint nose pytest coverage"
-exec { "install common python2 libraries":
-  command => "python2 -m ensurepip; python2 -m pip install --upgrade $python_libs ansible",
-  require => Package['python'],
-}
-exec { "install common python3 libraries":
-  command => "python3 -m ensurepip; python3 -m pip install --upgrade $python_libs doorstop gdm mine",
-  require => Package['python3'],
+# Install Python versions
+$python2 = '2.7.8'
+$python3 = '3.4.2'
+python::version { "$python2": }
+python::version { "$python3": }
+
+# Set the global version of Python
+class { 'python::global':
+  version => "$python3"
 }
 
-# documentation tools
+# Install common tools
+$python_libs = "pip virtualenv"
+exec { "install common python2 libraries":
+  command => "pip2 install --upgrade $python_libs ansible",
+}
+exec { "install common python3 libraries":
+  command => "pip3 install --upgrade $python_libs doorstop gdm mine",
+}
+
+# Install documentation tools
 package { 'pandoc':
   ensure => installed,
   provider => homebrew,
