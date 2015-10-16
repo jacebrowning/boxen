d12e0ae8f6751f604b642e92081c128fc5027508
Jace Browning <jacebrowning@gmail.com>
2015-10-16 16:06:38 -0400

    Add projects

--- modules/people/manifests/modustri_ci.pp
+++ modules/people/manifests/modustri_ci.pp
@@ -10,5 +10,20 @@ class people::modustri_ci {
     provider => git,
     ensure   => 'origin/HEAD',
   }
+  repository { "${boxen::config::srcdir}/modustri-uc-ios":
+    source => 'modustri/uc-mobile',
+    provider => git,
+    ensure   => 'origin/HEAD',
+  }
+  repository { "${boxen::config::srcdir}/modustri-uc-web":
+    source => 'modustri/uc-web',
+    provider => git,
+    ensure   => 'origin/HEAD',
+  }
+  repository { "${boxen::config::srcdir}/modustri-uc":
+    source => 'modustri/uc',
+    provider => git,
+    ensure   => 'origin/HEAD',
+  }
 
 }
