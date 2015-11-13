88839d698b6adc0b15ef536d0bcecbb38ab790bd
Jace Browning <jacebrowning@gmail.com>
2015-11-13 11:44:46 -0500

    Remove version requirements for repos

--- modules/people/manifests/modustri_ci.pp
+++ modules/people/manifests/modustri_ci.pp
@@ -9,22 +9,18 @@ class people::modustri_ci {
   repository { "${boxen::config::srcdir}/modustri-api":
     source => 'modustri/api',
     provider => git,
-    ensure   => 'origin/HEAD',
   }
   repository { "${boxen::config::srcdir}/modustri-uc-ios":
     source => 'modustri/uc-mobile',
     provider => git,
-    ensure   => 'origin/HEAD',
   }
   repository { "${boxen::config::srcdir}/modustri-uc-web":
     source => 'modustri/uc-web',
     provider => git,
-    ensure   => 'origin/HEAD',
   }
   repository { "${boxen::config::srcdir}/modustri-uc":
     source => 'modustri/uc',
     provider => git,
-    ensure   => 'origin/HEAD',
   }
 
   # TeamCity
