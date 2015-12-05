e2cb71f11d25503ab7cc3f86b7fd537fc935a58c
Jace Browning <jacebrowning@gmail.com>
2015-12-04 21:17:45 -0500

    Use ruby 2.2.3 and install bundler in all rubies

--- manifests/ruby.pp
+++ manifests/ruby.pp
@@ -1,3 +1,10 @@
-ruby::version { '2.1.4': }
+$version = '2.2.3'
+ruby::version { $version: }
 
-class { 'ruby::global': version => '2.1.4' }
+class { 'ruby::global': version => $version }
+
+ruby_gem { 'bundler for all rubies':
+  gem          => 'bundler',
+  version      => '~> 1.0',
+  ruby_version => '*',
+}
