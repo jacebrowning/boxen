2300cc7e0651446270e2e4711d5424ded8fa4b33
Jace Browning <jacebrowning@gmail.com>
2015-12-21 22:12:42 -0500

    Specify exact dependency versions

--- modules/people/manifests/jacebrowning.pp
+++ modules/people/manifests/jacebrowning.pp
@@ -66,22 +66,28 @@ class people::jacebrowning {
   python::package { "doorstop for $python3":
     package => 'doorstop',
     python  => $python3,
-    version => '>= 0.8',
+    version => '== 0.8',
   }
   python::package { "mine for $python3":
     package => 'mine',
     python  => $python3,
-    version => '>= 0.3',
+    version => '== 0.3',
   }
   python::package { "curmit for $python3":
     package => 'curmit',
     python  => $python3,
-    version => '>= 1.0',
+    version => '== 1.0',
   }
   python::package { "thefuck for $python3":
     package => 'thefuck',
     python  => $python3,
-    version => '>= 3.1',
+    version => '== 3.1',
+  }
+
+  python::package { "dropthebeat for $python3":
+    package => 'dropthebeat',
+    python  => $python3,
+    version => '== 0.1',
   }
 
 }
