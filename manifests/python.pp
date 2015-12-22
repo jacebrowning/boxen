beb4e05871357a82c7a4dcc555acb62523393749
Jace Browning <jacebrowning@gmail.com>
2015-12-22 17:48:56 -0500

    Update to GDM 0.7

--- manifests/python.pp
+++ manifests/python.pp
@@ -93,7 +93,7 @@ python::package { "ansible for $python2":
 python::package { "gdm for $python3":
   package => 'gdm',
   python  => $python3,
-  version => '== 0.7.dev3',
+  version => '== 0.7',
 }
 python::package { "httpie for $python3":
   package => 'httpie',
