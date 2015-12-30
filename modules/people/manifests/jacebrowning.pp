ff5e347abb62e5e0d04cb95c8a7660e43e50db1f
Jace Browning <jacebrowning@gmail.com>
2015-12-30 17:13:11 -0500

    Update to mine 0.4

--- modules/people/manifests/jacebrowning.pp
+++ modules/people/manifests/jacebrowning.pp
@@ -71,7 +71,7 @@ class people::jacebrowning {
   python::package { "mine for $python3":
     package => 'mine',
     python  => $python3,
-    version => '>=0.3.dev4,<0.4',
+    version => '>=0.4,<0.5',
   }
   python::package { "curmit for $python3":
     package => 'curmit',
