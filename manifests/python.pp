b0a36b5a8ae70fab87abdd49bfbfd6b9a17db91d
Jace Browning <jacebrowning@gmail.com>
2016-01-21 21:27:58 -0500

    Update GDM to 0.8.1

--- manifests/python.pp
+++ manifests/python.pp
@@ -109,7 +109,7 @@ python::package { "ansible for $python2":
 python::package { "gdm for $python3":
   package => 'gdm',
   python  => $python3,
-  version => '>=0.8,<0.9',
+  version => '>=0.8.1,<0.9',
 }
 python::package { "httpie for $python3":
   package => 'httpie',
