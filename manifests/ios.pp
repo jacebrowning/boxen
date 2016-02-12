8c953b369eb420aca0b9908e3cea80806e62015f
Jace Browning <jacebrowning@gmail.com>
2016-02-12 06:51:43 -0500

    Limit cocoapods installation to the current ruby

--- manifests/ios.pp
+++ manifests/ios.pp
@@ -1,13 +1,5 @@
-# install tools
-ruby_gem { 'cocoapods for all rubies':
-  gem          => 'cocoapods',
-  ruby_version => '*',
-}
-
-# test tools
 class { 'xctool':
   version => '0.2.7',
 }
 
-# analysis tools
 include oclint
--- manifests/ruby.pp
+++ manifests/ruby.pp
@@ -6,11 +6,17 @@ class { 'ruby::global': version => $ruby_version }
 
 ruby_gem { 'bundler for all rubies':
   gem          => 'bundler',
-  version      => '~> 1.11',
+  version      => '1.11.2',
   ruby_version => '*',
 }
 
-ruby_gem { 'travis for all rubies':
+ruby_gem { 'cocoapods for current ruby':
+  gem          => 'cocoapods',
+  version      => '0.39.0',
+  ruby_version => $ruby_version,
+}
+
+ruby_gem { 'travis for current ruby':
   gem          => 'travis',
   ruby_version => $ruby_version,
 }
