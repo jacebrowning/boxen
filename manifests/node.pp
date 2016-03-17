b38db91cb913817798e67eca586a33024a2e95b9
Jace Browning <jacebrowning@gmail.com>
2016-03-17 14:18:08 -0400

    Add svg2png-command

--- manifests/node.pp
+++ manifests/node.pp
@@ -4,19 +4,26 @@ class { 'nodejs::global':
   version => $node_version
 }
 
-npm_module { "bower for ${node_version}":
+npm_module { "bower for global node":
   module       => 'bower',
   version      => '~> 1.7.2',
   node_version => $node_version,
 }
-npm_module { "diff-so-fancy for default node":
+npm_module { "diff-so-fancy for global node":
   module       => 'diff-so-fancy',
+  node_version => $node_version,
+}
+npm_module { "svg2png-command for global node":
+  module       => 'svg2png-command',
+  version      => '~> 1.0.2',
   node_version => $node_version,
 }
 
 include phantomjs
 
-phantomjs::version { '1.9.8': }
+$phantom_version = "1.9.8"
+
+phantomjs::version { $phantom_version: }
 phantomjs::version { '2.1.1': }
 
-phantomjs::global { '1.9.8': }
+phantomjs::global { $phantom_version: }
