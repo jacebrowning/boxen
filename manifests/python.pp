35ea48cc04d22af9f7714c01be64639813a793cd
Jace Browning <jacebrowning@gmail.com>
2016-05-10 09:41:02 -0400

    Update GitMan to 0.11

--- manifests/python.pp
+++ manifests/python.pp
@@ -85,7 +85,7 @@ python::package { "ansible for $python2":
 python::package { "gitman for $python3":
   package => 'gitman',
   python  => $python3,
-  version => '>= 0.10, < 0.11',
+  version => '>= 0.11, < 0.12',
 }
 python::package { "httpie for $python3":
   package => 'httpie',
