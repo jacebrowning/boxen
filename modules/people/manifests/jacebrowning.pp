c413e6bb122c98858fb6c29a8238e9596884302a
Jace Browning <jacebrowning@gmail.com>
2016-07-02 17:18:08 -0400

    Update mine to 0.6

--- modules/people/manifests/jacebrowning.pp
+++ modules/people/manifests/jacebrowning.pp
@@ -76,7 +76,7 @@ class people::jacebrowning {
   python::package { "mine for $python3":
     package => 'mine',
     python  => $python3,
-    version => '>=0.5,<0.6',
+    version => '>=0.6,<0.7',
   }
   python::package { "curmit for $python3":
     package => 'curmit',
